f1cb198cf2e5fa22ae627b2e3aa392f5f8ecd2ed
sairamsreejith0 <30547601+sairamsreejith0@users.noreply.github.com>
2025-03-05 12:25:58 -0600

    Update main.py

--- app/main.py
+++ app/main.py
@@ -3,11 +3,10 @@ from .routers import auth, gadgets  # Import routers
 from .database import Base, engine
 from app.routers.auth import get_current_user
 
-# Create DB tables
+
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="IMF Gadget API", version="1.0")
 
-# Include Authentication & Gadgets Routes
 app.include_router(auth.router, prefix="/auth", tags=["Authentication"])
 app.include_router(gadgets.router,tags=["Gadgets"],dependencies=[Depends(get_current_user)])
